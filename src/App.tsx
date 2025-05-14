eda6ef6d2952219ee1ab34e68ee55c2f7d17fcc3
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-14 09:58:19 +0900

    May-14

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
