a89e9bc4c7e2265a5177fcbd558cd800a4da1cb1
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-16 18:25:33 +0900

    May-16

--- src/CleanUp.tsx
+++ src/CleanUp.tsx
@@ -1,9 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import React, { useState, useEffect } from 'react'
 
 const CleanUp: React.FC = () => {
     const [currentNum, setCurrentNum] = useState(0);
 
-    return <div>{currentNum}</div>
-}
+    const incrementNum = () => {
+        console.log("Mouse event invoked !");
+        setCurrentNum((preNumber) => preNumber + 1);
+    };
+
+    useEffect(() => {
+        console.log("useEffect in CleanUp invoked !");
+        window.addEventListener("mousedown", incrementNum);
+
+        return () => {
+            console.log("Cleanup invoked !");
+            window.removeEventListener("mousedown", incrementNum);
+        };
+    }, []);
+    return <div>{currentNum}</div>;
+};
 
 export default CleanUp
